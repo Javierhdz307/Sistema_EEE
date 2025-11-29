6034cb182982d5353f090e44d2b6565bbe90c8da
Javierhdz307 <135680395+Javierhdz307@users.noreply.github.com>
2025-11-29 05:50:47 -0600

    Fix template directory path

--- sistema_EEE/settings.py
+++ sistema_EEE/settings.py
@@ -90,14 +90,20 @@ WSGI_APPLICATION = 'sistema_EEE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",conn_max_age=600,
-        ssl_require=False
-    )
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    # Base local
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
