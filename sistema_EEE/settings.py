3fd3101dbdc084cf5dbd17fd1efd6a19cb54ba33
Javierhdz307 <135680395+Javierhdz307@users.noreply.github.com>
2025-11-29 04:25:13 -0600

    Fix template directory path

--- sistema_EEE/settings.py
+++ sistema_EEE/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'sistema_EEE.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'TEMPLATES'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
