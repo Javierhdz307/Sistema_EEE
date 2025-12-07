e77f2761ef960dc1bb1f02baaebf3dcfb99156d6
Javierhdz307 <135680395+Javierhdz307@users.noreply.github.com>
2025-12-07 07:01:08 -0600

    Modulo Reportes

--- Examenes/admin.py
+++ Examenes/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Examen, PAEI
 
 # Register your models here.
+@admin.register(Examen)
+class ExamenAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'tipo_examen', 'fecha_realizado', 'personal', 'estado', 'archivo_link')
+
+    def archivo_link(self, obj):
+        if obj.archivo_examen:
+            return f"<a href='{obj.archivo_examen.url}' target='_blank'>Ver archivo</a>"
+        return "Sin archivo"
+    archivo_link.allow_tags = True
+    archivo_link.short_description = "Archivo"
+
+@admin.register(PAEI)
+class PAEIAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'fecha_realizado', 'personal', 'estado', 'archivo_link')
+
+    def archivo_link(self, obj):
+        if obj.archivo_paei:
+            return f"<a href='{obj.archivo_paei.url}' target='_blank'>Ver archivo</a>"
+        return "Sin archivo"
+    archivo_link.allow_tags = True
+    archivo_link.short_description = "Archivo"
--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import ExamenTipo
-from Examenes.models import Examen, PAEI
 
 # Register your models here.
 @admin.register(ExamenTipo)
@@ -9,24 +8,3 @@ class ExamenTipoAdmin(admin.ModelAdmin):
     search_fields = ("nombre",)
     list_filter = ("nombre",) 
 # Register your models here.
-@admin.register(Examen)
-class ExamenAdmin(admin.ModelAdmin):
-    list_display = ('alumno', 'tipo_examen', 'fecha_realizado', 'personal', 'estado', 'archivo_link')
-
-    def archivo_link(self, obj):
-        if obj.archivo_examen:
-            return f"<a href='{obj.archivo_examen.url}' target='_blank'>Ver archivo</a>"
-        return "Sin archivo"
-    archivo_link.allow_tags = True
-    archivo_link.short_description = "Archivo"
-
-@admin.register(PAEI)
-class PAEIAdmin(admin.ModelAdmin):
-    list_display = ('alumno', 'fecha_realizado', 'personal', 'estado', 'archivo_link')
-
-    def archivo_link(self, obj):
-        if obj.archivo_paei:
-            return f"<a href='{obj.archivo_paei.url}' target='_blank'>Ver archivo</a>"
-        return "Sin archivo"
-    archivo_link.allow_tags = True
-    archivo_link.short_description = "Archivo"
