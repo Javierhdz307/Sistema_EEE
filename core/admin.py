57037f3bb3e8d22468f747a76b68b995760edf77
Javierhdz307 <135680395+Javierhdz307@users.noreply.github.com>
2025-12-07 06:29:07 -0600

    Modulo PAEI

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ExamenTipo
+from Examenes.models import Examen
 
 # Register your models here.
 @admin.register(ExamenTipo)
@@ -8,3 +9,8 @@ class ExamenTipoAdmin(admin.ModelAdmin):
     search_fields = ("nombre",)
     list_filter = ("nombre",) 
 # Register your models here.
+@admin.register(Examen)
+class ExamenAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'fecha_realizado', 'personal', 'archivo')
+    search_fields = ('alumno__nombre', 'alumno__apellido')
+    list_filter = ('fecha_realizado', 'personal')
